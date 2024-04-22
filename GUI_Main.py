a3ef9856c644fb20b75e051a29d3196fc462e6e7
gimbab2002 <gimbab2002@gmail.com>
2024-04-22 22:37:34 +0900

    The program was changed to be run GUI_basic_info.py into GUI_Main.py. ÇÁ·α׷¥ÀÌ GUI_basic_info.py¿¡¼­ ½ÇÇàµǾú´ø °ÍÀ» GUI_Main.py¿¡¼­ ½ÇÇàµǵµ·Ï ¹ٲÞ.

--- GUI_Main.py
+++ GUI_Main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
+import userInfo
 import json
+import os
 
 class MainScreen(tk.Frame):
     def __init__(self, master=None):
@@ -49,6 +51,7 @@ class MainScreen(tk.Frame):
         if user_info:
             info_str = f"성별: {user_info['gender']}\n인바디 점수: {user_info['inbody_score']}\n적정 체중: {user_info['ideal_weight']} kg\n지방 조절 수치: {user_info['fat_control']} kg\n근육 조절 수치: {user_info['muscle_control']} kg\n나이: {user_info['age']} 세"
             messagebox.showinfo("사용자 정보", info_str)
+   
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -61,8 +64,18 @@ class Application(tk.Frame):
         self.main_menu = MainScreen(self.master)
         self.main_menu.grid(sticky="nsew")  # Align the main menu frame to expand
 
-root = tk.Tk()
-root.geometry("800x600")
-root.title("Health Kitchen")
-app = Application(master=root)
-root.mainloop()
+def check_user_info_and_launch():
+    exists, user_info = userInfo.read_user_info()  # Call function from userInfo module
+    if not exists:
+        messagebox.showinfo("알림", "이 프로그램은 당신의 인바디 '분석 평가'(결과지의 우측 부분)의 일부 항목값을 입력받아 그 값을 토대로 적정 체중이 되도록 돕거나 유지시켜주는 프로그램 입니다. 따라서 다음 질문에 답 하기 전에 먼저 인바디 검사를 받고, 해당 검사지를 출력하여 준비해 주시기 바랍니다. 만약 인바디 검사지가 준비되셨다면, 질문에 솔직하게 답변 해 주세요.")
+        os.system('python GUI_basic_info.py')  # Launch user info input window
+    else:
+        root = tk.Tk()
+        root.geometry("800x600")
+        root.title("Health Kitchen")
+        app = Application(master=root)
+        root.mainloop()
+
+if __name__ == "__main__":
+    check_user_info_and_launch()    
+
