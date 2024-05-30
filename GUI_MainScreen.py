191bf816de8c89ec2d32e6d03c09f54ecb37f3d3
GyuMin Lee <lovekm0121@naver.com>
2024-05-31 02:02:28 +0900

    Modify comments and add lines.

--- GUI_MainScreen.py
+++ GUI_MainScreen.py
@@ -90,7 +90,7 @@ class MainScreen(tk.Frame):
         messagebox.showinfo("알림", "이 프로그램은 당신의 인바디 '분석 평가'(결과지의 우측 부분)의 일부 항목값을 입력받아 그 값을 토대로 적정 체중이 되도록 돕거나 유지시켜주는 프로그램 입니다. 따라서 다음 질문에 답 하기 전에 먼저 인바디 검사를 받고, 해당 검사지를 출력하여 준비해 주시기 바랍니다. 만약 인바디 검사지가 준비되셨다면, 질문에 솔직하게 답변 해 주세요.")
 
     def progress_info(self):
-        # Create a new window for ExerciseTracker
+        # 운동진행상황을 보여주기 위한 새 창 열기
         progress_window = tk.Toplevel(self.master)
         ExerciseTracker(master=progress_window, main_screen=self).grid(sticky="nsew")
 
@@ -107,7 +107,7 @@ class MainScreen(tk.Frame):
                         f"체지방률: {user_info['body_fat']}\n"
                         f"현재상태: {user_info['status']}\n"
                         f"선택한 음식: {user_info['selected_food']['food']}\n"
-                        f"유통기한: {user_info['limit_time']}"
+                        f"유통기한: {user_info['limit_time']}\n"
                         f"레벨: {user_info['level']}\n"
                         f"만든 음식 수: {user_info['made_food_count']}")
             messagebox.showinfo("사용자 정보", info_str)
