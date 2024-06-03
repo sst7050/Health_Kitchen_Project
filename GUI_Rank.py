75ad912d6c4e7197fcf33bbf9f5dc6db1b1dfbe8
HyunSeok <sst7050@naver.com>
2024-06-03 17:57:46 +0900

    Created_Rank_widgets

--- GUI_Rank.py
+++ GUI_Rank.py
@@ -8,6 +8,16 @@ class RankImageFrame(tk.Frame):
         self.switch_frame_callback = switch_frame_callback
         self.create_Rank_widgets()  # 위젯 생성
 
+def create_Rank_widgets(self):  # 음식 리스트 및 위젯 생성
+        Rank_list = {
+            "주방 견습생": {"image": "img/food/Burger/Burger.png", "duration": "2개월", "price": "20000원", "detail_name": "햄버거"},
+            "초급 요리사": {"image": "img/food/Chicken/Chicken.png", "duration": "2개월", "price": "20000원", "detail_name": "치킨"},
+            "중급 요리사": {"image": "img/food/Pasta/Pasta.png", "duration": "2개월", "price": "20000원", "detail_name": "파스타"},
+            "주방장": {"image": "img/food/Pilaf/Pilaf.png", "duration": "2개월", "price": "19000원", "detail_name": "필라프"},
+            "요리의 달인": {"image": "img/food/Pizza/Pizza.png", "duration": "2개월", "price": "20000원", "detail_name": "피자"},
+            "요리왕 비룡": {"image": "img/food/Steak/Steak.png", "duration": "2개월", "price": "18000원", "detail_name": "스테이크"},
+            "고든 램지": {"image": "img/food/Stew/Stew.png", "duration": "2개월", "price": "19000원", "detail_name": "스튜"}
+        }
 
 
 
