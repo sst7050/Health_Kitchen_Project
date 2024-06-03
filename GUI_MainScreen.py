ccb080961b3aa4df40949b5f4f7cd68a78bbd523
HyunSeok <sst7050@naver.com>
2024-06-03 17:53:09 +0900

    Rank image widget

--- GUI_MainScreen.py
+++ GUI_MainScreen.py
@@ -104,8 +104,8 @@ class MainScreen(tk.Frame):
         fridge_window = tk.Toplevel(self.master)
         RefrigeratorScreen(master=fridge_window, main_screen=self).grid(sticky="nsew")
 
-    def start_info_input(self):
-        messagebox.showinfo("알림", "이 프로그램은 당신의 인바디 '분석 평가'(결과지의 우측 부분)의 일부 항목값을 입력받아 그 값을 토대로 적정 체중이 되도록 돕거나 유지시켜주는 프로그램 입니다. 따라서 다음 질문에 답 하기 전에 먼저 인바디 검사를 받고, 해당 검사지를 출력하여 준비해 주시기 바랍니다. 만약 인바디 검사지가 준비되셨다면, 질문에 솔직하게 답변 해 주세요.")
+    #def start_info_input(self):
+    #   messagebox.showinfo("알림", "이 프로그램은 당신의 인바디 '분석 평가'(결과지의 우측 부분)의 일부 항목값을 입력받아 그 값을 토대로 적정 체중이 되도록 돕거나 유지시켜주는 프로그램 입니다. 따라서 다음 질문에 답 하기 전에 먼저 인바디 검사를 받고, 해당 검사지를 출력하여 준비해 주시기 바랍니다. 만약 인바디 검사지가 준비되셨다면, 질문에 솔직하게 답변 해 주세요.")
 
     def progress_info(self):
         # 운동진행상황을 보여주기 위한 새 창 열기
