fd63febbea218d2ae80fb669999e7f0b30299992
gimbab2002 <gimbab2002@gmail.com>
2024-06-04 00:41:56 +0900

    Remove showing details of food in GUI_Sel_Food.py. And remove useless code in GUI_Main.py

--- GUI_Sel_Food.py
+++ GUI_Sel_Food.py
@@ -47,55 +47,22 @@ class FoodSelectionFrame(tk.Frame):
             messagebox.showinfo("Error", "올바르지 않은 경로입니다. 처음부터 다시 시작해 주세요.") #json파일이 없을 경우
             self.master.after(100, self.relaunch_Main)
             return
-        
-        user_info['selected_food'] = { # 선택한 음식 정보 추가
-            'food': food,
-            'details': info
-        }
-        #제한시간 추가
-        timelimit = datetime.now() + timedelta(weeks= 1)
-        timelimit = timelimit.strftime("%Y-%m-%d %H:%M:%S")
-        user_info['limit_time'] = timelimit
-        user_info['ingredient'] = []
-        userInfo.save_user_info(user_info)  # 정보 저장
-        self.switch_frame_callback(FoodDetailFrame, info["detail_name"], info)  # 상세 정보 화면으로 전환
-
-class FoodDetailFrame(tk.Frame):
-    def __init__(self, master, go_back_callback):
-        super().__init__(master)
-        self.go_back_callback = go_back_callback
-
-    def display_food(self, detail_name, details):
-        self.pack_forget()  # 이전 내용 지우기
-        self.master.title(f"{detail_name} 상세 정보")  # 창 제목 설정
-
-        img = tk.PhotoImage(file=details["image"])  # 이미지 로드
-        img_label = tk.Label(self, image=img)  # 이미지 레이블
-        img_label.image = img  # 이미지 참조 유지
-        img_label.pack(side=tk.TOP, pady=20)  # 이미지 레이블 위치 설정
-
-        info_text = f"이름: {detail_name}\n예상 기간: {details['duration']}\n가격: {details['price']}"  # 상세 정보 텍스트
-        info_label = tk.Label(self, text=info_text)  # 정보 레이블
-        info_label.pack(side=tk.TOP, pady=20)  # 정보 레이블 위치 설정
-
-        prev_button = tk.Button(self, text="이전", command=self.go_back)  # '이전' 버튼
-        prev_button.pack(side=tk.LEFT, padx=20, pady=20)  # 버튼 위치 설정
-
-        next_button = tk.Button(self, text="다음", command=self.go_next)  # '다음' 버튼
-        next_button.pack(side=tk.RIGHT, padx=20, pady=20)  # 버튼 위치 설정
-
-    def go_back(self):
-        # 이전 화면으로 돌아가기
-        self.go_back_callback(FoodSelectionFrame)
-
-    def go_next(self):
-        # 선택 확인받기
-        response = messagebox.askyesno("확인", "선택한 음식으로 진행하시겠습니까?")
+        response = messagebox.askyesno("확인", f"{food}을(를) 선택하시겠습니까?")
         if response:
-            self.master.destroy()  # 현재 창 닫기
-            subprocess.run('python GUI_MainScreen.py') # 메인 GUI 실행
+            user_info['selected_food'] = { # 선택한 음식 정보 추가
+                'food': food,
+                'details': info
+            }
+            #제한시간 추가
+            timelimit = datetime.now() + timedelta(weeks= 1)
+            timelimit = timelimit.strftime("%Y-%m-%d %H:%M:%S")
+            user_info['limit_time'] = timelimit
+            user_info['ingredient'] = []
+            userInfo.save_user_info(user_info)  # 정보 저장
+            self.master.destroy()
+            subprocess.run('python GUI_MainScreen.py')
         else:
-            self.go_back() # 선택을 취소하고 음식 리스트 화면으로 돌아가기
+            self.switch_frame_callback(FoodSelectionFrame)
 
 class MainApp(tk.Tk):
     def __init__(self):
