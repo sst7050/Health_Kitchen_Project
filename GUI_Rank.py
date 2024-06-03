052b4e1361a555c67c5a86d6b60eea34b591e5cf
HyunSeok <sst7050@naver.com>
2024-06-03 20:01:01 +0900

    modified_GUI_Rank.py

--- GUI_Rank.py
+++ GUI_Rank.py
@@ -10,13 +10,13 @@ class RankImageFrame(tk.Frame):
 
 def create_Rank_widgets(self):  # 음식 리스트 및 위젯 생성
         Rank_list = {
-            "주방 견습생": {"image": "img/food/Burger/Burger.png", "duration": "2개월", "price": "20000원", "detail_name": "햄버거"},
-            "초급 요리사": {"image": "img/food/Chicken/Chicken.png", "duration": "2개월", "price": "20000원", "detail_name": "치킨"},
-            "중급 요리사": {"image": "img/food/Pasta/Pasta.png", "duration": "2개월", "price": "20000원", "detail_name": "파스타"},
-            "주방장": {"image": "img/food/Pilaf/Pilaf.png", "duration": "2개월", "price": "19000원", "detail_name": "필라프"},
-            "요리의 달인": {"image": "img/food/Pizza/Pizza.png", "duration": "2개월", "price": "20000원", "detail_name": "피자"},
-            "요리왕 비룡": {"image": "img/food/Steak/Steak.png", "duration": "2개월", "price": "18000원", "detail_name": "스테이크"},
-            "고든 램지": {"image": "img/food/Stew/Stew.png", "duration": "2개월", "price": "19000원", "detail_name": "스튜"}
+            "주방 견습생": {"image": "img/chef/kitchen_apprentice.png", "foods": "default", "detail_explanation": "요리를 해 본적이 없는 초보 견습생"},
+            "초급 요리사": {"image": "img/chef/beginner_chef.png", "foods": "1개", "detail_explanation": "좀 치기 시작한 요리사"},
+            "중급 요리사": {"image": "img/chef/intermediate_chef.png", "foods": "3개", "detail_explanation": "짬이 차서 부점장까지 오른 요리사"},
+            "주방장": {"image": "img/food/chef/head_chef.png", "foods": "6개","detail_explanation": "점장으로부터 식당을 승계받은 요리사"},
+            "요리의 달인": {"image": "img/chef/master_of_cooking.png", "foods": "10개", "detail_explanation": "식당을 미슐랭 가이드에 등재시킨 요리사"},
+            "요리왕 비룡": {"image": "img/chef/Cooking_King_Biryong.png", "foods": "15개", "detail_explanation": "TV 맛집탐방 프로그램에 출연하게 된 요리사"},
+            "고든 램지": {"image": "img/chef/gordon_ramsey.png", "foods": "21개","detail_explanation": "전설의 요리사"}
         }
 
 
