76cee8ad8a3b9461ba764a143c74e76c03259bf1
gimbab2002 <gimbab2002@gmail.com>
2024-06-08 15:50:51 +0900

    Making setUp method.

--- unittest/test_GUI_Refrigerator.py
+++ unittest/test_GUI_Refrigerator.py
@@ -9,3 +9,17 @@ import os
 from PIL import Image
 from GUI_Refrigerator import RefrigeratorScreen
 
+class TestRefrigeratorScreen(unittest.TestCase):
+    def setUp(self):
+        # 임시 JSON 데이터 생성
+        self.user_info = {
+            "selected_food": {
+                "details": {
+                    "ingredient_path": "temp_ingredients"
+                }
+            },
+            "ingredient": ["test_img1.png", "test_img2.png"]
+        }
+        with open("user_info.json", "w", encoding="utf-8") as file:
+            json.dump(self.user_info, file)
+        
