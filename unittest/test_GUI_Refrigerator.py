3e0caabeaee8158b49c70190df86dbffd113b556
gimbab2002 <gimbab2002@gmail.com>
2024-06-08 15:50:06 +0900

    making GUI_Regrigerator's testFile. importing some libraries.

---
+++ unittest/test_GUI_Refrigerator.py
@@ -0,0 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+import tkinter as tk
+import json
+import os
+from PIL import Image
+from GUI_Refrigerator import RefrigeratorScreen
+
